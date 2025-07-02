2e7ad0b495da5c91f0fcfd6500d18d8eacfcdb4d
FApew <112774976+FApew@users.noreply.github.com>
2025-07-02 14:27:24 +0200

    FixTest3.3

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,7 +1,7 @@
 import * as THREE from "/three/build/three.module.js"
-//import Stats from "/three/examples/jsm/libs/stats.module.js"
-//import { GLTFLoader } from "/three/examples/jsm/loaders/GLTFLoader.js"
-//import { OrbitControls } from '/three/examples/jsm/controls/OrbitControls.js'
+import Stats from "/three/examples/jsm/libs/stats.module.js"
+import { GLTFLoader } from "/three/examples/jsm/loaders/GLTFLoader.js"
+import { OrbitControls } from '/three/examples/jsm/controls/OrbitControls.js'
 
 //import * as CANNON from 'https://cdn.jsdelivr.net/npm/cannon-es@0.20.0/+esm'
 //import cannonDebugger from 'https://cdn.jsdelivr.net/npm/cannon-es-debugger@1.0.0/+esm'
